312695cdfb314f282b0d91b587fd71a83e0c4355
matthoneycutt <mbhoneycutt@gmail.com>
2013-06-27 19:20:06 -0500

    Updated deployment scripts to work with Powershell 3.0.

--- Deploy/publish.ps1
+++ Deploy/publish.ps1
@@ -8,17 +8,7 @@ if ((Get-Module psake) -eq $null) {
 	$loadedPsake = $true;
 }
 
-$loadedPscx = $false;
-if ((Get-Module pscx) -eq $null) {
-	import-module .\extensions\pscx	
-	$loadedPscx = $true;
-}
-
 invoke-psake "$($PackageName).ps1" -taskList Publish
-
-if ($loadedPscx) { 
-	remove-module pscx
-}
 	
 if ($loadedPsake) {
 	remove-module psake
